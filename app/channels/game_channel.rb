4e19535def364ebe9ff5217b01ab09775c4dc498
Niklas Hildebarndt <n.g.hildebrandt@gmail.com>
2018-08-30 15:40:54 +0700

    streaming using connection uuid

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,9 +1,5 @@
 class GameChannel < ApplicationCable::Channel
   def subscribed
-    # ugly as fuck
-
-    binding.irb
-    game = current_user.games.last
-    stream_for game
+    stream_for "#{uuid}"
   end
 end
